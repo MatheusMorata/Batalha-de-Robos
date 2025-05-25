a7faa88820c62e3f82f7151b7d03e0a564e67d90
MatheusMorata <matlucar09@hotmail.com>
2025-05-25 14:57:34 -0300

    FEAT: Pequenas mudancas Robo.py

--- Robo.py
+++ Robo.py
@@ -1,12 +1,13 @@
 class Robo:
 
+    # Construtor
     def __init__(self, F, E, V):
         self.forca = F
         self.energia = E
         self.velocidade = V
 
-    
+    # Retorna dos atributos do robô
     def status(self,):
-        print('Forca: ', self.forca)
+        print('Força: ', self.forca)
         print('Energia: ', self.energia)
         print('Velocidade: ', self.velocidade)
