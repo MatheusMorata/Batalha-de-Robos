68862fc0a25b206004180e6534e2217fc62d2e0a
MatheusMorata <matlucar09@hotmail.com>
2025-05-25 16:48:42 -0300

    FEAT: Alocando robos no tabuleiro

--- Tabuleiro.py
+++ Tabuleiro.py
@@ -1,14 +1,25 @@
 class Tabuleiro:
-
     def __init__(self, a=20, l=40):
         self.altura = a
         self.largura = l
+        self.robos = []  # Lista de tuplas: (robo, x, y)
 
-    # Exibe o tabuleiro
+    def adicionarRobos(self, robo, x, y):
+        if 0 <= x < self.largura and 0 <= y < self.altura:
+            self.robos.append((robo, x, y))
+        else:
+            raise Exception("Posição do robô fora dos limites do tabuleiro.")
+
     def Apresentar(self):
+        # Cria matriz vazia
+        matriz = [[' ' for _ in range(self.largura)] for _ in range(self.altura)]
 
-        print('+' + '-' * self.largura + '+')  
-        for _ in range(self.altura):
-            print('|' + ' ' * self.largura + '|') 
-        print('+' + '-' * self.largura + '+')  
+        # Posiciona robôs no tabuleiro
+        for idx, (robo, x, y) in enumerate(self.robos):
+            matriz[y][x] = str(idx)  
 
+        # Imprime com moldura
+        print('+' + '-' * self.largura + '+')
+        for linha in matriz:
+            print('|' + ''.join(linha) + '|')
+        print('+' + '-' * self.largura + '+')
--- main.py
+++ main.py
@@ -2,15 +2,20 @@ from Robo import Robo
 from Tabuleiro import Tabuleiro
 
 # Debuggando
-"""
+
 try:
-    vascaino = Robo(10,100,5)
-    vascaino.status()
-    vascaino.energia = 80
-    vascaino.status()
+    # Criação dos robôs
+    r1 = Robo(5, 50, 3)
+    r2 = Robo(8, 90, 2)
+
+    # Criação do tabuleiro
+    tab = Tabuleiro()
+
+    # Adiciona robôs nas posições desejadas
+    tab.adicionarRobos(r1, x=5, y=3)
+    tab.adicionarRobos(r2, x=10, y=6)
+
+    # Exibe o tabuleiro com os robôs
+    tab.Apresentar()
 except Exception as e:
     print(e)
-"""
-
-visualizador = Tabuleiro()
-visualizador.Apresentar()
