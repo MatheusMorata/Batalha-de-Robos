e4c6bf2c064aaa99ba7b283065742eeebb61452e
MatheusMorata <matlucar09@hotmail.com>
2025-05-29 13:06:32 -0300

    FEAT: Pequenas mudanças na main

--- main.py
+++ main.py
@@ -3,22 +3,25 @@ from Tabuleiro import Tabuleiro
 
 # Debuggando
 
-try:
-    
-    # Criação do tabuleiro
-    tab = Tabuleiro()
+if __name__ == '__main__':
+    try:
+        
+        # Criação do tabuleiro
+        tab = Tabuleiro()
 
-    # Criação dos robôs
-    r1 = Robo('A', 5, 50, 3, 5, 3, 'vivo')
-    r2 = Robo('B', 8, 90, 2, 10, 6, 'vivo')
-    r3 = Robo('C', 7, 20, 1, 15, 2, 'vivo')  
+        # Criação dos robôs
+        r1 = Robo('A', 5, 50, 3, 5, 3, 'vivo')
+        r2 = Robo('B', 8, 90, 2, 10, 6, 'vivo')
+        r3 = Robo('C', 7, 20, 1, 15, 2, 'vivo')  
+        r4 = Robo('D', 10, 10, 5, 22, 13, 'vivo')  
 
-    # Adiciona robôs no tabuleiro
-    tab.adicionarRobo(r1)
-    tab.adicionarRobo(r2)
-    tab.adicionarRobo(r3)
+        # Adiciona robôs no tabuleiro
+        tab.adicionarRobo(r1)
+        tab.adicionarRobo(r2)
+        tab.adicionarRobo(r3)
+        tab.adicionarRobo(r4)
 
-    # Exibe o tabuleiro
-    tab.Apresentar()
-except Exception as e:
-    print(e)
+        # Exibe o tabuleiro
+        tab.Apresentar()
+    except Exception as e:
+        print(e)
