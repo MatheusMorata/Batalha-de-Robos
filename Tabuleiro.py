7b1d80ed866a36f70d88a370996dd77384eebb7b
MatheusMorata <matlucar09@hotmail.com>
2025-05-30 14:24:24 -0300

    FEAT: Memória compartilhada para o tabuleiro e os robos

--- Tabuleiro.py
+++ Tabuleiro.py
@@ -1,33 +1,28 @@
 class Tabuleiro:
-    def __init__(self, a=20, l=40):
-        self.altura = a
-        self.largura = l
-        self.robos = []
-        self.baterias = []
-
-    def adicionarRobo(self, robo):
-        if 0 <= robo.x < self.largura and 0 <= robo.y < self.altura:
-            self.robos.append(robo)
-        else:
-            raise Exception("Posição do robô fora dos limites do tabuleiro.")
-
-    def adicionarBateria(self, bateria):
-        if 0 <= bateria.x < self.largura and 0 <= bateria.y < self.altura:
-            self.baterias.append(bateria)
-        else:
-            raise Exception("Posição da bateria fora dos limites do tabuleiro.")
+    def __init__(self, tabuleiro, robos, baterias, linhas, colunas):
+        self.tabuleiro = tabuleiro
+        self.robos = robos
+        self.baterias = baterias
+        self.linhas = linhas
+        self.colunas = colunas
 
     def Apresentar(self):
-        matriz = [[' ' for _ in range(self.largura)] for _ in range(self.altura)]
+        # Cria uma matriz para visualização correta (linhas x colunas)
+        matriz = [[' ' for _ in range(self.colunas)] for _ in range(self.linhas)]
 
+        # Adiciona robôs na matriz
         for robo in self.robos:
             if robo.status == 'vivo':
-                matriz[robo.y][robo.x] = str(robo.id)[0]  # Mostra o primeiro caractere do ID
+                if 0 <= robo.y < self.linhas and 0 <= robo.x < self.colunas:
+                    matriz[robo.y][robo.x] = str(robo.id)[0]
 
+        # Adiciona baterias na matriz
         for bateria in self.baterias:
-            matriz[bateria.y][bateria.x] = str(bateria.id)[0]  # Mostra o primeiro caractere do ID
+            if 0 <= bateria.y < self.linhas and 0 <= bateria.x < self.colunas:
+                matriz[bateria.y][bateria.x] = str(bateria.id)[0]
 
-        print('+' + '-' * self.largura + '+')
+        # Imprime o tabuleiro
+        print('+' + '-' * self.colunas + '+')
         for linha in matriz:
             print('|' + ''.join(linha) + '|')
-        print('+' + '-' * self.largura + '+')
+        print('+' + '-' * self.colunas + '+')
--- main.py
+++ main.py
@@ -1,39 +1,47 @@
 from Robo import Robo
 from Tabuleiro import Tabuleiro
 from Bateria import Bateria
-import random 
+from multiprocessing import Array, Manager
+import random
 
 # Debuggando
 if __name__ == '__main__':
-    robos = []
+
     try:
+        # Variáveis 
+        linhas = 40
+        colunas = 20
+        tabuleiro = Array('i',linhas * colunas) # Memória compartilhada para o tabuleiro
         
-        # Criação do tabuleiro
-        tab = Tabuleiro()
+        with Manager() as manager:
 
-        # Criando e alocando no tabuleiro os robôs
-        for i in range(0,4):
-            # Atributos do robô
-            id = chr(ord('a') + i)     
-            forca = random.randint(1,10)   
-            energia = random.randint(10,100) 
-            velocidade = random.randint(1,5)
-            posicao_x = random.randint(0,39)
-            posicao_y = random.randint(0,19)
+            robos = manager.list() # Memória compartilhada para os robôs
+            baterias = manager.list() # Memória compartilhada para as baterias
 
-            # O robô foi criado e posicionado no tabuleiro
-            tab.adicionarRobo(Robo(id, forca, energia, velocidade, posicao_x, posicao_y))
-            
-        # Criando e alocando no tabuleiro as baterias
-        for i in range(0,10):
-            posicao_x = random.randint(0,39)
-            posicao_y = random.randint(0,19)
+            for i in range(0,4):
+
+                # Atributos do robô
+                id = chr(ord('a') + i)     
+                forca = random.randint(1,10)   
+                energia = random.randint(10,100) 
+                velocidade = random.randint(1,5)
+                posicao_x = random.randint(0,39)
+                posicao_y = random.randint(0,19)
+
+                # Robô criado e alocado na memória compartilhada
+                robos.append(Robo(id, forca, energia, velocidade, posicao_x, posicao_y))
+
+            for i in range(0,10):
+
+                # Atributos da bateria
+                posicao_x = random.randint(0,39)
+                posicao_y = random.randint(0,19)
         
-            # A bateroa foi criada e posicionada no tabuleiro
-            tab.adicionarBateria(Bateria(posicao_x, posicao_y))
-            
-        # Exibe o tabuleiro
-        tab.Apresentar()
+                # Bateria criada e alocada na memória compartilhada
+                baterias.append(Bateria(posicao_x, posicao_y))
+
+            tab = Tabuleiro(tabuleiro, robos, baterias, colunas, linhas)
+            tab.Apresentar() # Apresenta o tabuleiro
 
     except Exception as e:
         print(e)
