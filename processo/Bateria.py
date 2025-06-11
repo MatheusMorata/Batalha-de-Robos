604047433d98c43d7d700b9108bc04d30c5baf6a
MatheusMorata <matlucar09@hotmail.com>
2025-06-11 15:39:10 -0300

    FEAT: Adicionei função criarBateria

--- processo/Bateria.py
+++ processo/Bateria.py
@@ -4,7 +4,7 @@ class Bateria:
     # Construtor
     def __init__(self):
         # Atributos
-        self.id = ''
+        self.id = 'ϟ'
         self.energia = 20
         self.X = random.randint(0,39)
         self.Y = random.randint(0,19)
