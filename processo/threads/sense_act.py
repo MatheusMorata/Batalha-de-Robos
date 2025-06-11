740f2a3a1ed18c8edb27318013e5fda41d104237
MatheusMorata <matlucar09@hotmail.com>
2025-06-11 14:57:36 -0300

    FEAT: Adicionei mover na thread sense_act

--- processo/threads/sense_act.py
+++ processo/threads/sense_act.py
@@ -1,6 +1,7 @@
 def sense_act(robo):
     while(robo.status == 'vivo'):
-        print(f'Robo {robo.id} iniciou com energia {robo.energia}')
+        print(f'Robo {robo.id} coordenadas({robo.X}, {robo.Y})')
+        robo.mover()
         robo.energia -= 1
         if(robo.energia < 1):
             robo.status = 'morto'
