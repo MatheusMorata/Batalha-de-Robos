f6e7943cf6484b9381b620702c103491bd06838d
MatheusMorata <matlucar09@hotmail.com>
2025-06-14 14:11:10 -0300

    FEAT: Thread encontra robôs e baterias (debug)

--- processo/threads/sense_act.py
+++ processo/threads/sense_act.py
@@ -2,5 +2,12 @@ def sense_act(robo, tabuleiro):
     while(robo.status == 'vivo'):
         robo.mover(tabuleiro)
         robo.energia -= 1
+
+        if(robo.roboProximo() == True):
+            print(f'Robo {robo.id} encontrou um robô')
+            
+        if(robo.bateriaProxima() == True):
+            print(f'Robo {robo.id} encontrou uma bateria')
+
         if(robo.energia < 1):
             robo.status = 'morto'
