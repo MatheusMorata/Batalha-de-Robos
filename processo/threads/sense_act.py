132893b43346859efd3ebedde026b9d1b36c2dcb
MatheusMorata <matlucar09@hotmail.com>
2025-06-14 14:43:26 -0300

    REFACTOR: Excluindo visualização

--- processo/threads/sense_act.py
+++ processo/threads/sense_act.py
@@ -1,26 +1,6 @@
-from time import time, sleep
-from view.Tabuleiro import imprimir_tabuleiro 
-
 def sense_act(robo, tabuleiro):
-    frame_time = 1 / 10  # ~10 FPS
-
     while robo.status == 'vivo':
-        start_time = time()
-
         robo.mover(tabuleiro)
         robo.energia -= 1
-
-        imprimir_tabuleiro(tabuleiro)
-
-        # if robo.roboProximo(tabuleiro):
-        #     print(f'Robô {robo.id} encontrou um robô')
-
-        # if robo.bateriaProximo(tabuleiro):
-        #     print(f'Robô {robo.id} encontrou uma bateria')
-
         if robo.energia < 1:
             robo.status = 'morto'
-
-        elapsed_time = time() - start_time
-        sleep_time = max(0, frame_time - elapsed_time)
-        sleep(sleep_time)
