bc731f1eb82a93d79a835967f959449956a5a54c
MatheusMorata <matlucar09@hotmail.com>
2025-06-14 15:25:24 -0300

    FEAT: Baterias com memória dinâmica

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from multiprocessing import Process
-from utils.Robos import *
-from utils.Baterias import *
+from utils.Utils import *
 from multiprocessing import Array
 
 if __name__ == '__main__':
@@ -15,10 +14,11 @@ if __name__ == '__main__':
     energia_robos = Array('i', numRobos)
     posicao_robos = Array('i', numRobos)
     status_robos = Array('u', 'V' * numRobos) # V = Vivo // M = Morto // C = Carregando // B = Batalhando
+    posicao_baterias = Array('i', numBaterias)
 
     # Preenchendo com os valores
     robos_ids = ids(robos_ids)
     forca_robos = forca(forca_robos)
     energia_robos = energia(energia_robos)
     posicao_robos = posicao(posicao_robos)
-
+    posicao_baterias = posicao(posicao_baterias)
