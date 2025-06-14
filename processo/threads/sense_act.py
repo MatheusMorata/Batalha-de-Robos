d76c468eb49890305961fbb78171a650ed1e8ad8
MatheusMorata <matlucar09@hotmail.com>
2025-06-14 15:42:19 -0300

    FEAT: Pequenas mudanças

--- processo/threads/sense_act.py
+++ processo/threads/sense_act.py
@@ -1,6 +1,6 @@
 def sense_act(robo, tabuleiro, ID, forca, energia, posicao, status):
     while(status[robo.numRobo] != 'M'):
-        print(f'Robô {ID[robo.numRobo]} energia {energia[robo.numRobo]}')  
         energia[robo.numRobo] -= 1
+        
         if(energia[robo.numRobo] < 1):
             status[robo.numRobo] = 'M'
