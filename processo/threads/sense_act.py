74f2eeae2eae1701162cbb9d116d4c82056b3c88
MatheusMorata <matlucar09@hotmail.com>
2025-06-15 16:58:42 -0300

    CLEANUP: Removendo código inoperante

--- processo/threads/sense_act.py
+++
@@ -1,6 +0,0 @@
-def sense_act(robo, tabuleiro, ID, forca, energia, posicao, status):
-    while(status[robo.numRobo] != 'M'):
-        energia[robo.numRobo] -= 1
-        
-        if(energia[robo.numRobo] < 1):
-            status[robo.numRobo] = 'M'
